0239a546eecfd4bd2cb98b9e5e5c51bb765fc87b
Joao Fernandes Lima <joaolima@deliz.com.br>
2020-02-19 15:34:06 -0300

    readd classe

---
+++ src/main/kotlin/br/com/correios/webservice/postagem/AssuntoPIMaster.kt
@@ -0,0 +1,17 @@
+package br.com.correios.webservice.postagem
+
+import javax.xml.bind.annotation.XmlAccessType
+import javax.xml.bind.annotation.XmlAccessorType
+import javax.xml.bind.annotation.XmlType
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "assuntoPIMaster", propOrder = ["codigo", "descricao", "tipo"])
+class AssuntoPIMaster {
+
+    var codigo: Int? = null
+
+    var descricao: String? = null
+
+    var tipo: String? = null
+
+}
